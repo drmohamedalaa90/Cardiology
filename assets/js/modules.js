639de3966b09c3631fb58918169f1cbe6f4b9233
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-07-29 02:39:12 +0300

    Update modules.js

--- assets/js/modules.js
+++ assets/js/modules.js
@@ -1,5 +1,5 @@
 import { supabaseClient } from "./supabase-client.js";
-import { protectAndRender } from "./session-ui.js?v=3.0.0";
+import { protectAndRender } from "./session-ui.js?v=4.0.0";
 
 
 const grid =
