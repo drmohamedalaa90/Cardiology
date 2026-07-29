5313daa68837a073cac74ebf71dd62cc42b71af4
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-07-30 01:24:40 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,29 @@
 export const ACL_CONFIG = {
-  supabaseUrl: "https://zcuseloltsmzohpwmnvo.supabase.co",
-  supabasePublishableKey: "sb_publishable_YJNKDMu-fbyGMLUT46uUkg_IULOVaZH",
-  siteBase: "/Cardiology/",
-  appName: "ACL; THE EXPERT EDITION",
+  supabaseUrl:
+    "https://zcuseloltsmzohpwmnvo.supabase.co",
+
+  supabasePublishableKey:
+    "sb_publishable_YJNKDMu-fbyGMLUT46uUkg_IULOVaZH",
+
+  siteBase:
+    "/Cardiology/",
+
+  vapidPublicKey:
+    "BFM-lV_Uf3RvPDVLWZv_bEZgAEW7I-o6KlsJUbUuEDUhOG5QCat0EmD3hRx11WGcyjLmpkYvemP895pMuqPeBjA",
+
+  appName:
+    "ACL; THE EXPERT EDITION",
+
   modules: [
-    { id: "ppci", title: "Primary PCI Fundamentals", href: "modules/ppci/" }
+    {
+      id:
+        "ppci",
+
+      title:
+        "Primary PCI Fundamentals",
+
+      href:
+        "modules/ppci/"
+    }
   ]
 };
