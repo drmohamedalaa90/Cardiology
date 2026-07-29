4f1141f13e78ca06ed37539d53ac1483e0dd5f95
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-07-29 10:31:08 +0300

    Update modules.js

--- assets/js/modules.js
+++ assets/js/modules.js
@@ -1,5 +1,5 @@
 import { supabaseClient } from "./supabase-client.js";
-import { protectAndRender } from "./session-ui.js?v=4.2.0";
+import { protectAndRender } from "./session-ui.js?v=4.3.0";
 
 
 
